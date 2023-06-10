3f7bf16949bb3a478e7e53f0a9c0b45f92fc6be6
Ahmed <kaziahmed.r@solutelabs.com>
2023-06-10 13:27:14 +0530

    back to normal

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withTM = require('next-transpile-modules')(['d3-color']);
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["jherr-pokemon.s3.us-west-1.amazonaws.com", "archives.bulbagarden.net"]
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      'd3-color': false
-    };
-
-    return config;
   }
 }
 
