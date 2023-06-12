371796c81ae1b5fdef4ed6066bbdf321f1998dc6
Ahmed <kaziahmed.r@solutelabs.com>
2023-06-12 10:14:03 +0530

    webpack

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["jherr-pokemon.s3.us-west-1.amazonaws.com", "archives.bulbagarden.net"]
-  }
-}
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        module: false,
+      };
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
 
-
-
+module.exports = nextConfig;
